f5b5a06c760926e3eef792618275b1521ff41100
achinup <achinthyau@gmail.com>
2025-03-04 13:01:28 +0530

    test problem resolved

--- demo/src/test/java/com/example/demo/ExApplicationTests.java
+++ demo/src/test/java/com/example/demo/ExApplicationTests.java
@@ -2,8 +2,9 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.example.auth.ExApplication;
 
-@SpringBootTest
+@SpringBootTest(classes = ExApplication.class)
 class ExApplicationTests {
 
 	@Test
